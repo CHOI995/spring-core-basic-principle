21a972573d5766269918fa78dd2de6d46251bdce
wsc <cwsmirs@naver.com>
2021-12-30 15:02:37 +0900

    강의 완료 커밋

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -2,6 +2,7 @@ package hello.core.web;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
--- src/main/java/hello/core/web/LogDemoService.java
+++ src/main/java/hello/core/web/LogDemoService.java
@@ -1,5 +1,6 @@
 package hello.core.web;
 
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 import hello.core.common.MyLogger;
@@ -12,7 +13,7 @@ public class LogDemoService {
 	private final MyLogger myLogger;
 	
 	public void logic(String id) {
-		myLogger.log("service id= " + id);		
+	    myLogger.log("service id= " + id);		
 	}
 
 }
